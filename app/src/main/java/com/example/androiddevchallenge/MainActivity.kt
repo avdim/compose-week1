14af9ce6c21f91b807c9e66f1118fa60acf2210f
avdim <avdim@mail.ru>
2021-02-28 14:35:37 +0300

    update state

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,19 +18,47 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+
+data class AppState(
+    val message: String,
+    val count: Int
+)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val mutableStateFlow = MutableStateFlow(
+            AppState("hello compose", 0)
+        )
+
+        fun updateState() {
+            val old = mutableStateFlow.value
+            mutableStateFlow.value = old
+                .copy(
+                    count = old.count + 1
+                )
+        }
+
         setContent {
+            val myState = mutableStateFlow.collectAsState()
             MyTheme {
-                MyApp()
+                MyApp(myState.value) {
+                    updateState()
+                }
             }
         }
     }
@@ -38,17 +66,30 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
+fun MyApp(appState: AppState, onClick: () -> Unit = {}) {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Column {
+            Text(
+                text = appState.message,
+                modifier = Modifier
+                    .border(4.dp, color = MaterialTheme.colors.primary)
+                    .padding(24.dp)
+            )
+            Button(onClick = onClick) {
+                Text("click me ${appState.count}")
+            }
+        }
     }
 }
+
+
+fun fakeState() = AppState("fake", 0)
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
     MyTheme {
-        MyApp()
+        MyApp(fakeState())
     }
 }
 
@@ -56,6 +97,6 @@ fun LightPreview() {
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
-        MyApp()
+        MyApp(fakeState())
     }
 }
